669399c1005ace6601182dc1e911298a376997fb
estridha <estridhassbring@gmail.com>
2023-02-20 01:03:15 +0100

    fixed error in last commit

--- website/src/views/LandingPage.vue
+++ website/src/views/LandingPage.vue
@@ -1,70 +1,40 @@
 <template>
   <div class="landing-page-container">
-    <h1 class="landing-page-desc">Ready to make your business <br/> look like it deserves</h1>
-	<router-link to="/about"><BaseButton buttonClass="secondary" text="Enter"/></router-link>
-    <div class="video-container">
-			<video src="https://traversymedia.com/downloads/video.mov" autoplay muted loop></video>
-		</div>
-    
+    <h1 class="landing-page-desc">
+      Photographer <br />
+      Kevan Doyle
+    </h1>
+    <router-link to="/about"
+      ><BaseButton buttonClass="secondary" text="Enter"
+    /></router-link>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import BaseButton from '@/components/BaseButton.vue'
+import BaseButton from "@/components/BaseButton.vue";
 
 export default {
-  name: 'LandingPage',
+  name: "LandingPage",
   components: {
-    BaseButton
-  }
-}
+    BaseButton,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Megrim&display=swap');
 
 
-
-.landing-page-desc {
-	padding-top: 30vh;
-	padding-bottom: 4rem;
-  color: white;
-}
-
-.video-container {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-    z-index: -1;
-	overflow: hidden;
-	background: var(--primary-color) url('https://traversymedia.com/downloads/cover.jpg') no-repeat center
-		center;
-}
-
-.video-container video {
-	min-width: 100%;
-	min-height: 100%;
-  position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	object-fit: cover;
-}
-
-.video-container:after {
-	content: '';
-	z-index: 1;
-	height: 100%;
-	width: 100%;
-	top: 0;
-	left: 0;
-	background: rgba(0, 0, 0, 0.5);
-	position: absolute;
-}
-
-img {
+.landing-page-container {
+  overflow: hidden;
+  height: 100vh;
   width: 100vw;
+  background-image: url(@/assets/images/prints/fine-art/fine-art\ \(7\).jpg);
+  background-size: cover;
+}
+.landing-page-desc {
+  font-family: 'Megrim', cursive;
+  padding-top: 30vh;
 }
 </style>
